724c72a8338deb52ddef82897e2bed3d737949aa
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2022-12-21 16:06:17 +0300

    Add private and public methods for Person class

--- person.rb
+++ person.rb
@@ -8,9 +8,13 @@ class Person
   attr_accessor :name, :age
   attr_reader :id
 
+  private
+
   def of_age?
     @age >= 18
   end
+
+  public
 
   def can_use_services?
     of_age? || @parent_permission
